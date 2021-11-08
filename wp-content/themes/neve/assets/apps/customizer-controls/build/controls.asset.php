1d2fc714c84d61ec538e69199e9dc8c12e88d8e2
rangasake <rangaswamysake@gmail.com>
2021-11-08 17:15:39 +0000

    home updated

---
+++ wp-content/themes/neve/assets/apps/customizer-controls/build/controls.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill', 'wp-primitives'), 'version' => '2eb271ee74b12b6b165ed9fc4f3254ce');
---
+++ wp-content/themes/neve/assets/apps/dashboard/build/dashboard.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-api', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'ad7e9ef7bdc82ddcc6d5bf4935bff60d');
